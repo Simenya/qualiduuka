c510933e42fb7896b109683e770577d889a1b3f9
William <bobliam20gitlm@gmail.com>
2022-07-03 12:48:10 +0300

    Changed the scanner orientation, enabled scanner to add an item to the list but not more than 1 item and configured the scanner validation to only add items with price tags

--- Quality Duuka/app/src/main/java/com/app/smartshamba/MainActivity.kt
+++ Quality Duuka/app/src/main/java/com/app/smartshamba/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.app.smartshamba
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.widget.Button
-import android.widget.GridView
-import android.widget.ImageView
-import android.widget.ListView
+import android.view.View
+import android.widget.*
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -15,28 +15,53 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.text.isDigitsOnly
 import com.app.smartshamba.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
+import com.journeyapps.barcodescanner.CaptureActivity
+import org.json.JSONException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    var scanCode: ImageView? = null
+
+    private lateinit var scanProduct:ImageView
+    private lateinit var cardScaProduct:CardView
+    lateinit var layoutItems:RelativeLayout
+    private var itemValue:TextView? = null
+    lateinit var bottomView:RelativeLayout
+    lateinit var total:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //calling the list adapter function
-        setListData(this)
-    //Scan Qr Code
-        scanCode = findViewById(R.id.scanQrId)
-        scanCode!!.setOnClickListener {
-            startActivity(Intent(this, ScanCode::class.java))
+        scanProduct = findViewById(R.id.scanQrCodeId)
+        cardScaProduct = findViewById(R.id.cardView1)
+        layoutItems = findViewById(R.id.linearLayoutItems)
+        itemValue = findViewById(R.id.codeContentsId)
+        bottomView = findViewById(R.id.textBtnBottomId)
+        total = findViewById(R.id.totalId)
 
+        //Scan Qr Code
+        scanProduct.setOnClickListener {
+            cardScaProduct.visibility = View.GONE
+            layoutItems.visibility = View.GONE
+            bottomView.visibility = View.GONE
+
+            cameraTask()
         }
 
+
+        //calling the list adapter function
+        //setListData(this)
+
+        //Navigation Drawer
         setSupportActionBar(binding.appBarMain.toolbar)
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
@@ -59,24 +84,12 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun setListData(context: Context){
+    fun setListData(context: Context, itemName:String, itemPrice:String){
         //Generating A data source
-        val itemNames:Array<String> = arrayOf(
-            "Crisps",
-            "Fanta Soda 300ml",
-            "Pizza",
-            "Colgate",
-            "Pacolator",
-            "Water Bottle"
-        )
-        val itemPrices:Array<String> = arrayOf(
-            "2000",
-            "3000",
-            "20000",
-            "30000",
-            "40000",
-            "25000"
-        )
+        val itemNames = ArrayList<String>()
+        itemNames.add(itemName)
+        val itemPrices = ArrayList<String>()
+        itemPrices.add(itemPrice)
 
         val gridView = findViewById<ListView>(R.id.listViewId)
 
@@ -90,6 +103,104 @@ class MainActivity : AppCompatActivity() {
         val adapter = ItemAdapter(context, listOfArray)
         gridView.adapter = adapter
 
+    }
+
+    //setting up the scanner
+    private fun hasCameraAccess(): Boolean {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+    private fun requestCameraPermissions(){
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 101)
+    }
+
+    private fun cameraTask() {
+
+        if (hasCameraAccess()) {
+            scanQr()
+        } else {
+            requestCameraPermissions()
+            scanQr()
+        }
+    }
+
+    private fun scanQr(){
+        val qrScanner = IntentIntegrator(this)
+        qrScanner.setPrompt("QR code Scanning...")
+        qrScanner.setCameraId(0)
+        qrScanner.setOrientationLocked(true)
+        qrScanner.setBeepEnabled(true)
+        qrScanner.captureActivity = CaptureActivity::class.java
+        qrScanner.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_SHORT).show()
+                itemValue!!.text = ""
+                cardScaProduct.visibility = View.VISIBLE
+                layoutItems.visibility = View.VISIBLE
+                bottomView.visibility = View.VISIBLE
+
+            } else {
+
+                try {
+                    val content = result.contents
+                    val splitContent = content.split(" ")
+                    val itemName = splitContent[0]
+                    val itemPrice = splitContent[splitContent.lastIndex]
+                    var totals = 0
+
+                    if (splitContent.size > 1){
+                        if (itemPrice.isDigitsOnly()){
+                            for (i in content){
+                                setListData(this, itemName, itemPrice)
+                            }
+                            totals += itemPrice.toInt()
+                            total.text = totals.toString()
+
+                            cardScaProduct.visibility = View.VISIBLE
+                            layoutItems.visibility = View.VISIBLE
+                            bottomView.visibility = View.VISIBLE
+
+                        }else{
+                            Toast.makeText(this, "This Item Has no Price Tag", Toast.LENGTH_LONG).show()
+                            cardScaProduct.visibility = View.VISIBLE
+                            layoutItems.visibility = View.VISIBLE
+                            bottomView.visibility = View.VISIBLE
+                        }
+                    }else{
+                        if (splitContent.isEmpty()){
+                            Toast.makeText(this, "This Item Has No \nName and Price Tag", Toast.LENGTH_LONG).show()
+                            cardScaProduct.visibility = View.VISIBLE
+                            layoutItems.visibility = View.VISIBLE
+                            bottomView.visibility = View.VISIBLE
+                        }else{
+                            Toast.makeText(this, "This Item Has no Price Tag", Toast.LENGTH_LONG).show()
+                            cardScaProduct.visibility = View.VISIBLE
+                            layoutItems.visibility = View.VISIBLE
+                            bottomView.visibility = View.VISIBLE
+                        }
+                    }
+
+                } catch (exception: JSONException) {
+                    Toast.makeText(this, exception.localizedMessage, Toast.LENGTH_SHORT).show()
+                    itemValue!!.text = ""
+                    cardScaProduct.visibility = View.VISIBLE
+                    layoutItems.visibility = View.VISIBLE
+                    bottomView.visibility = View.VISIBLE
+                }
+
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+
+//        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
+//            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+//        }
     }
 
 }
